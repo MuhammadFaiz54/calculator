a5ca238340ae8118447c13f5a297a5f651811763
MuhammadFaiz54 <70813541+MuhammadFaiz54@users.noreply.github.com>
2021-01-02 23:53:39 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+function getnum (num){
+
+   
+
+    var result = document.getElementById("result");
+
+    result.value += num;
+
+}
+function clean(){
+    var result = document.getElementById("result");
+
+    result.value = ""
+}
+function getresult(){
+    var result = document.getElementById("result")
+
+    result.value = eval(result.value)
+}
